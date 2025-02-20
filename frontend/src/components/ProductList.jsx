af36e094d82aaa0fc32c517408b8b56e0a41dbda
Niten Kumawat <nitenkumawat@gmail.com>
2025-02-21 00:30:41 +0530

    more look added

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -151,7 +151,7 @@ function ProductList() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
-            <div key={product._id} className="bg-white shadow-md rounded-lg p-4">
+            <div key={product._id} className="bg-white shadow-md rounded-lg p-4 transition-all duration-300 transform hover:scale-105">
               <img src={product.imageURL} alt={product.name} className="w-full h-40 object-contain rounded-md mb-4" />
               <h3 className="text-lg font-bold">{product.name}</h3>
               <p className="text-gray-600">{product.description}</p>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,8 +6,8 @@ function HomePage() {
   const [refresh, setRefresh] = useState(false);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">E-Commerce Store</h1>
+    <div className="w-screen mx-auto p-4 bg-gradient-to-br from-yellow-100 to-red-100">
+      <h1 className="text-3xl font-bold text-center mb-6">SmartFilter Ecommerce</h1>
       <ProductForm onProductAdded={() => setRefresh(!refresh)} />
       <ProductList key={refresh} />
     </div>
